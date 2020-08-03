5d48d26c47c56237676197d53cf28f53969b9659
z0ggy <zoggy76@gmail.com>
2020-08-03 19:38:06 +0100

    Add few addressForm_Page locators

--- src/main/java/pages/addressForm_Page.java
+++ src/main/java/pages/addressForm_Page.java
@@ -1,4 +1,40 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class addressForm_Page {
+    public static WebDriver driver;
+
+    // Constructor
+    public addressForm_Page(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+    @FindBy(name = "alias")
+    WebElement alias;
+
+    @FindBy(name = "city")
+    WebElement city;
+
+    @FindBy(name = "postcode")
+    WebElement postcode;
+
+    @FindBy(css = "select[name='id_country']")
+    WebElement country;
+
+    @FindBy(xpath = "//option[text()='United Kingdom']")
+    WebElement countryChoose;
+
+    @FindBy(css = "input[name='phone']")
+    WebElement phone;
+
+    public void formFiller(String pageAlias){
+        alias.isEnabled();
+        alias.sendKeys(pageAlias);
+    }
 }
+
+
