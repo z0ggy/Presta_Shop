42713321accec5f21123885bb819de934846c0e6
z0ggy <zoggy76@gmail.com>
2020-08-08 18:54:13 +0100

    Clean code @ AddressForm_Page

--- src/main/java/pages/AddressForm_Page.java
+++ src/main/java/pages/AddressForm_Page.java
@@ -5,36 +5,60 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class AddressForm_Page {
-    public static WebDriver driver;
+import java.util.List;
 
-    // Constructor
+public class AddressForm_Page {
+    private static WebDriver driver;
+
     public AddressForm_Page(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
-    @FindBy(name = "alias")
-    WebElement alias;
 
-    @FindBy(name = "city")
-    WebElement city;
+    @FindBy(name = "alias") WebElement myAlias;
+    public void inputAlias(String alias) {
+        myAlias.click();
+        myAlias.sendKeys(alias);
+    }// Alias field
 
-    @FindBy(name = "postcode")
-    WebElement postcode;
+    @FindBy(name = "address1") WebElement myAddress;
+    public void inputAddress(String address) {
+        myAddress.click();
+        myAddress.sendKeys(address);
+    }// Address field
 
-    @FindBy(css = "select[name='id_country']")
-    WebElement country;
+    @FindBy(name = "postcode") WebElement myPostcode;
+    public void inputPostcode(String postcode) {
+        myPostcode.click();
+        myPostcode.sendKeys(postcode);
+    }// Zip/Postcode field
 
-    @FindBy(xpath = "//option[text()='United Kingdom']")
-    WebElement countryChoose;
+    @FindBy(name = "city") WebElement myCity;
+    public void inputCity(String city) {
+        myCity.click();
+        myCity.sendKeys(city);
+    }// City field
 
-    @FindBy(css = "input[name='phone']")
-    WebElement phone;
+    @FindBy(name = "id_country") WebElement myCountryField;
+    @FindBy(css = "select.form-control > option:nth-child(2)")
+    List<WebElement> myCountries;
+    public void chooseCountry() {
+        myCountryField.click();
+        myCountries.get(0).click();
+    }// Country Box
 
-    public void formFiller(String myAlias){
-       // alias.isEnabled();
-        alias.sendKeys(myAlias);
+    @FindBy(css = "button.btn.btn-primary") WebElement submit;
+    public void clickSubmitButton() {
+        submit.isEnabled();
+        submit.isDisplayed();
+        submit.click();
     }
-}
 
+    public String getMyAlias()    { return myAlias.getAttribute("value"); }
+    public String getMyAddress()  { return myAddress.getAttribute("value"); }
+    public String getMyPostcode() { return myPostcode.getAttribute("value"); }
+    public String getMyCity()     { return myCity.getAttribute("value"); }
+    public String getMyCountry()  { return myCountries.get(0).getText(); }
+
+}
 
