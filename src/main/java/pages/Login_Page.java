32dc79bd9e92fce0f1472f15873379eeedc0357b
z0ggy <zoggy76@gmail.com>
2020-08-08 20:35:31 +0100

    import @FindBy in Login_Page

--- src/main/java/pages/Login_Page.java
+++ src/main/java/pages/Login_Page.java
@@ -1,30 +1,26 @@
 package pages;
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class Login_Page {
-    private static WebDriver driver;
-
-
-    //constructor
     public Login_Page(WebDriver driver) {
-        this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
+
+    @FindBy(name = "email") WebElement loginInput;
+    @FindBy(name = "password") WebElement passwordInput;
+    @FindBy(id = "submit-login") WebElement signInButton;
+
     public void loginAs(String email, String password) {
-        WebElement loginInput = driver.findElement(By.name("email"));
         loginInput.click();
         loginInput.clear();
         loginInput.sendKeys(email);
-        WebElement passwordInput = driver.findElement(By.name("password"));
         passwordInput.click();
         passwordInput.clear();
         passwordInput.sendKeys(password);
-        WebElement signInButton = driver.findElement(By.id("submit-login"));
         signInButton.click();
-    }
-    public String getLoggedUsername() {
-        WebElement userName = driver.findElement(By.xpath("//a[@class='account']"));
-        return userName.getText();
     }
 }
