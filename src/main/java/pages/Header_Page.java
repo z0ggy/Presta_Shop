2715477c967c0c899beb4dd6ec0ec315b19ecd86
z0ggy <zoggy76@gmail.com>
2020-08-08 20:30:53 +0100

    Add Header_Page delete Home page

--- src/main/java/pages/Header_Page.java
+++ src/main/java/pages/Header_Page.java
@@ -6,15 +6,16 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class Header_Page {
-    public static WebDriver driver;
 
-    // Constructor
     public Header_Page(WebDriver driver) {
-        this.driver = driver;
         PageFactory.initElements(driver, this);
     }
-
     // View my customer account
-    @FindBy(css = "a.account") WebElement myAccount;
-    public void goToMyAccountPage() { myAccount.click();}
+    @FindBy(css = "a.account")
+    WebElement myAccount;
+
+    // Click on My Account
+    public void goToMyAccountPage() {
+        myAccount.click();
+    }
 }
