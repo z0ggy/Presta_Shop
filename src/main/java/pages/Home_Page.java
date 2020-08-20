de0dd154d2080074932cc6cbfb5094a583c79164
z0ggy <zoggy76@gmail.com>
2020-08-20 22:26:18 +0100

    added constructor to Home_Page

--- src/main/java/pages/Home_Page.java
+++ src/main/java/pages/Home_Page.java
@@ -1,5 +1,12 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
 public class Home_Page {
+
+    public Home_Page(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
 
 }
