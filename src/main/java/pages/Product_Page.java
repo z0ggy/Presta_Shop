763d371fa71791101f37de40c61a83cc86b216bc
z0ggy <zoggy76@gmail.com>
2020-08-23 23:26:23 +0100

    clean code

--- src/main/java/pages/Product_Page.java
+++ src/main/java/pages/Product_Page.java
@@ -19,28 +19,27 @@ public class Product_Page {
     @FindBy(className = "current-price")
     WebElement currentPrice;
 
-    //String priceBeforeDiscount = regularPrice.getText().substring(1);
-
     public double checkDiscount(double percent) {
-      double priceBeforeDiscount = Double.parseDouble(regularPrice.getText().substring(1));
-        return priceBeforeDiscount - priceBeforeDiscount * (percent/100);
+        double priceBeforeDiscount = Double.parseDouble(regularPrice.getText().substring(1));
+        return priceBeforeDiscount - priceBeforeDiscount * (percent / 100);
     }
 
     public void printPrice() {
-        double priceAfterDiscount = Double.parseDouble(currentPrice.getText().substring(1,7));
+        double priceAfterDiscount = Double.parseDouble(currentPrice.getText().substring(1, 7));
         System.out.println(regularPrice.getText().substring(1));
         //System.out.println(priceBeforeDiscount);
         Double priceBeforeDiscount = Double.parseDouble(regularPrice.getText().substring(1));
         System.out.println(priceBeforeDiscount);
         System.out.println(currentPrice.getText());
-        System.out.println(currentPrice.getText().substring(1,7));
+        System.out.println(currentPrice.getText().substring(1, 7));
         System.out.println(priceAfterDiscount);
     }
+
     public void checkProductIsOnDiscount() {
-        double priceAfterDiscount = Double.parseDouble(currentPrice.getText().substring(1,7));
+        double priceAfterDiscount = Double.parseDouble(currentPrice.getText().substring(1, 7));
         if (checkDiscount(percent) == priceAfterDiscount) {
             System.out.println("Price is on " + percent + " % " + "discount");
-        }else {
+        } else {
             Assert.fail("Price is not on discount");
         }
     }
