0a507bd4cc8ca7dd07996ed909032eac3e7b6f92
z0ggy <zoggy76@gmail.com>
2020-09-10 22:36:56 +0100

    added page factory for OrderConfirmation_Page

--- src/main/java/pages/OrderConfirmation_Page.java
+++ src/main/java/pages/OrderConfirmation_Page.java
@@ -1,4 +1,11 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
 public class OrderConfirmation_Page {
+
+    public OrderConfirmation_Page(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
 }
